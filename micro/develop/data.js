a9b7eb045c8937ff896984f82bd098bc820cc47e
github-action-benchmark <github@users.noreply.github.com>
2026-06-18 06:32:01 +0000

    add rotki backend micro benchmarks (develop) (pytest) benchmark result for 78d8a4ccf8f4795fd5ae0a6e7ea5ef3bc9bd94ca

--- micro/develop/data.js
+++ micro/develop/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781678912141,
+  "lastUpdate": 1781764321755,
   "repoUrl": "https://github.com/rotki/rotki",
   "entries": {
     "rotki backend micro benchmarks (develop)": [
@@ -50,6 +50,56 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000024439218349607584",
             "extra": "mean: 20.546368337368722 usec\nrounds: 8199"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Yábir Benchakhtir",
+            "username": "yabirgb",
+            "email": "git@yabirgb.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "78d8a4ccf8f4795fd5ae0a6e7ea5ef3bc9bd94ca",
+          "message": "Merge pull request #12436 from LefterisJP/mcp_gating\n\nGate MCP access behind Basic premium tier and up",
+          "timestamp": "2026-06-16T11:23:18Z",
+          "url": "https://github.com/rotki/rotki/commit/78d8a4ccf8f4795fd5ae0a6e7ea5ef3bc9bd94ca"
+        },
+        "date": 1781764321474,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "rotkehlchen/tests/benchmarks/test_hot_paths.py::test_history_event_db_serialization",
+            "value": 257.5428259966744,
+            "unit": "iter/sec",
+            "range": "stddev: 0.007320313953069169",
+            "extra": "mean: 3.88284937128442 msec\nrounds: 202"
+          },
+          {
+            "name": "rotkehlchen/tests/benchmarks/test_hot_paths.py::test_history_event_api_serialization",
+            "value": 179.58107204064308,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0009248688337081953",
+            "extra": "mean: 5.568515593746308 msec\nrounds: 96"
+          },
+          {
+            "name": "rotkehlchen/tests/benchmarks/test_hot_paths.py::test_fval_arithmetic",
+            "value": 1393.9508835372558,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000012898422935544443",
+            "extra": "mean: 717.3853912717674 usec\nrounds: 1329"
+          },
+          {
+            "name": "rotkehlchen/tests/benchmarks/test_hot_paths.py::test_events_filter_query_construction",
+            "value": 50599.87120216049,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000025045132048661432",
+            "extra": "mean: 19.762896154512394 usec\nrounds: 8426"
           }
         ]
       }
